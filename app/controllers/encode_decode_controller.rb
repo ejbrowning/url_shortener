83f10c28b79a740b0bd178eeecd20391da7c920f
E Browning <ebrowning2@wisc.edu>
2023-05-19 11:45:46 -0500

    changed base64 to urlsafe_base64 for long ids

--- app/controllers/encode_decode_controller.rb
+++ app/controllers/encode_decode_controller.rb
@@ -9,11 +9,11 @@ class EncodeDecodeController < ApplicationController
             render status: 400
         else
             #converting url to a shortened url using the base and a random base64
-            long_id = http_base + SecureRandom.base64(6).to_s
+            long_id = http_base + SecureRandom.urlsafe_base64(6).to_s
             # check to see if the id has already been used
             while UrlTable.find_by_url_id(long_id).present?
                 # generate a new shortened url while the current one already exists in the table
-                long_id = http_base + SecureRandom.base64(6).to_s 
+                long_id = http_base + SecureRandom.urlsafe_base64(6).to_s 
             end
             # making a new row in the table for the long_url and its id
             new_row = UrlTable.create(long_url:long, url_id:long_id)
